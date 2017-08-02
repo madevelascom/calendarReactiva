c11ef0689029c693379393623ca934d6d6baa7ab
Madelyne Velasco <m_velasco93@hotmail.com>
2017-08-02 15:30:43 -0500

    Reactiva calendar example

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php include_once('functions.php'); ?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>PHP Event Calendar by CodexWorld</title>
+<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<link type="text/css" rel="stylesheet" href="style.css"/>
+<script src="jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</head>
+<body>
+
+<div id="calendar_div">
+	<?php echo getCalender(); ?>
+</div>
+
+</body>
+</html>
